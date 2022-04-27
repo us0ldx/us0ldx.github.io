64f44be8f69d5784a8e9bc3e889f4afd7aaa020a
us0ldx <98390771+us0ldx@users.noreply.github.com>
2022-04-27 15:38:35 +0700

    Add files via upload

---
+++ js/protection.js
@@ -0,0 +1,24 @@
+	document.onkeydown = function(e) {
+		if (e.ctrlKey && (e.keyCode === 67 || e.keyCode === 86 || e.keyCode === 85 || e.keyCode === 117)) {}
+		if (e.shiftKey && (event.button == 2 || event.button == 3)) {}
+		return false;
+	};
+	function click() {
+		if (event.button == 2 || event.button == 3) {
+			oncontextmenu = 'return false';
+			function disableWheelScroll() {
+				if (document.body.addEventListener) document.body.addEventListener('DOMMouseScroll', blockWheel, false);
+				document.body.onmousewheel = blockWheel;
+			}
+			function blockWheel(event) {
+				if (!event) event = window.event;
+				if (event.stopPropagation) event.stopPropagation();
+				else event.cancelBubble = true;
+				if (event.preventDefault) event.preventDefault();
+				else event.returnValue = false;
+			}
+			disableWheelScroll();
+		}
+	}
+	document.onmousedown = click
+	document.oncontextmenu = new Function("return false;")
